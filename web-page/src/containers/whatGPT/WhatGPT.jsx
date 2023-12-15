b17ff894d3585511b55e79b1635e9f58f31b4e96
josealbertoruizr <josealbertoruizr@gmail.com>
2023-12-15 00:53:22 -0600

    whatGPT component

--- web-page/src/containers/whatGPT/WhatGPT.jsx
+++ web-page/src/containers/whatGPT/WhatGPT.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './WhatGPT.css'
+import  Feature  from '../../components/feature/Feature'
 
-function WhatGPT() {
+const WhatGPT = () => {
   return (
-    <div>WhatGPT</div>
+    <div className='gpt3__whatgpt3 section__margin ' id = "wgpt3">
+      <div className='gpt3_whatgpt3-feature'>
+        <Feature />
+      </div>
+      
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
