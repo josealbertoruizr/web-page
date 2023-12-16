0cc11d75c4625d0c9604be0a1157cd7310bcd4e6
josealbertoruizr <josealbertoruizr@gmail.com>
2023-12-15 21:36:31 -0600

    cta, blog, article, possibility components

--- web-page/src/components/cta/Cta.jsx
+++ web-page/src/components/cta/Cta.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Cta.css'
 
-function Cta() {
+const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request early access</p>
+        <h3>Register today and start</h3>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button typeof='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
--- web-page/src/containers/possibility/Possibility.jsx
+++ web-page/src/containers/possibility/Possibility.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Possibility.css'
+import possibility from '../../assets/possibility.png'
 
-function Possibility() {
+const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibility} alt='Possibility'/>
+      </div>
+
+      <div className='gpt3__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond our imagination</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>Request early access to get started</h4>
+      </div>
+    </div>
   )
 }
 
